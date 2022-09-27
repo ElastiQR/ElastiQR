4942544f23b6afb266b1c249a6a0445a16ca9900
hcurl <hcurl@vols.utk.edu>
2022-09-27 09:38:16 -0400

    gh-18 qrController

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const { createUserController, loginUserController} = require('../controllers/authController')
+const {createQRController} = require('../controllers/qrController')
 
+router.post('/createQR', createQRController)
 router.post('/createUser', createUserController)
 router.post('/login', loginUserController)
 
