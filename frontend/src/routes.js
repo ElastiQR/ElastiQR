64eecedafb34ed66fd8ca10599c8cd710eb47ff7
VaughanEric <emv212@gmail.com>
2022-10-05 12:27:28 -0400

    gh-27: Functionality, Layout, & Styling

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Route, HashRouter, Switch } from 'react-router-dom'
-import Main from './components/Main'
-import ExplorePage from './components/ExplorePage'
-import ScrollToTop from './components/ScrollTop'
+import React from 'react';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+import Main from './components/Main';
+import ExplorePage from './components/ExplorePage';
+import CreatePage from './components/CreatePage'
+import ScrollToTop from './components/ScrollTop';
 
 export default props => (
     <HashRouter>
@@ -10,6 +11,7 @@ export default props => (
         <Switch>
           <Route exact path='/' component={ Main } />
           <Route exact path='/my-qrs' component={ ExplorePage } />
+          <Route exact path='/create-qr' component={ CreatePage } />
         </Switch>
       </ScrollToTop>
     </HashRouter>
