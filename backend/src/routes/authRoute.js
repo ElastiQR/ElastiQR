2a775b8d4b4271b0bbf1083bdede9d9cf9dea98e
hcurl <hcurl@vols.utk.edu>
2022-10-10 16:15:18 -0400

    gh-41 GET request

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,14 +1,11 @@
 const router = require('express').Router()
-
 const { createUserController, loginUserController} = require('../controllers/authController')
-const {createQRController} = require('../controllers/qrController')
+const {createQRController, retrieveQRController} = require('../controllers/qrController')
 
-router.get('/getQRCodes', function(req,res){
-    var codes = request.body.codes;
-})
-
+router.get('/getQRCodes', retrieveQRController)
 
 router.post('/createQR', createQRController)
 router.post('/createUser', createUserController)
 router.post('/login', loginUserController)
 
+module.exports = router
