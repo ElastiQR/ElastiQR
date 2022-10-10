7a24b218f5541db57b555876c5cf065a9ff30f52
loganofneal <loneal7@vols.utk.edu>
2022-10-10 11:27:30 -0400

    more cors changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(express.json())
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* Public Routes */
 app.get("/", (req, res) => {
   res.send("Backend Running!");
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const cors = require("cors")
-
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
