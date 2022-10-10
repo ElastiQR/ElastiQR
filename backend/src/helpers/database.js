4693f11cdb86b542fa3f397b03b1244ac57b62ef
loganofneal <loneal7@vols.utk.edu>
2022-10-10 10:44:15 -0400

    added mising dotenv lib call to find .env file

--- backend/src/helpers/database.js
+++ backend/src/helpers/database.js
@@ -7,14 +7,16 @@ const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
 
+console.log("HOST: " + DB_HOST )
 // const INSTANCE_CONNECTION_NAME = process.env.INSTANCE_CONNECTION_NAME;
 
 const db = mysql.createPool({
-  connectionLimit: 5,
+  connectionLimit: 100,
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  port: "3306",
   // socketPath: `/cloudsql/${INSTANCE_CONNECTION_NAME}`,
 });
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 require("dotenv").config()
 const express = require("express");
 var bodyParser = require('body-parser')
+const db = require("./helpers/database")
+
+require('dotenv').config({ path: require('find-config')('.env') })
 
 /* Import Routes */
 const authRouter = require('./routes/authRoute')
@@ -12,11 +15,16 @@ app.use(bodyParser.json());
 
 /* Public Routes */
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Backend Running!");
 });
 
 app.use('/auth', authRouter)
 
 /* Start Application */
-const port = process.env.PORT
+const port = 3000;
 app.listen(port, ()=> console.log(`Server Started on port ${port}...`))
+
+db.getConnection( (err, connection)=> {
+  if (err) throw (err)
+  console.log ("DB connected successful: " + connection.threadId)
+})
