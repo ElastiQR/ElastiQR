1b0c526ee8ac09ad6913414162e43fad3805fbb4
loganoneal <loganofneal@gmail.com>
2022-11-16 11:26:17 -0500

    qr service fixes

--- backend/src/services/qrService.js
+++ backend/src/services/qrService.js
@@ -4,11 +4,10 @@ const cron = require("node-cron");
 const isReachable = require('is-reachable');
 
 
-validateLinksService = cron.schedule("*/60 * * * * *", function () {
-    var broken_count = 0;
-
+validateLinksService = cron.schedule("*/5 * * * *", function () {
     console.log("---------------------");
     console.log("NODE-CRON: validating all QR links");
+    console.log("---------------------");
 
     db.getConnection(async (err, connection) => {
         if (err) throw (err)
@@ -26,20 +25,14 @@ validateLinksService = cron.schedule("*/60 * * * * *", function () {
                 var url = result[i]['qrURL'];
                 var reachable = await isReachable(url);
 
-                if(!validLink) broken_count++;
-
                 if(reachable != validLink) {
                     const sqlUpdate = `UPDATE QRCodes SET validLink = ? WHERE qrID = ?`;
                     const update_query = mysql.format(sqlUpdate, [reachable, qrID]);
                     await connection.query (update_query);
                 }
             }
-            connection.end();
         })
-
     })
-    console.log("NODE-CRON: found " + broken_count + " broken links across all users");
-    console.log("---------------------");
 });
 
 module.exports = validateLinksService;
