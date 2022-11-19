6da46e84e1663df5791a2ce30dc28dbea0a43b4b
Caleb Fisher <cfishe36@vols.utk.edu>
2022-11-19 17:12:42 -0500

    ElastiQRgh-144: oauth backend integration

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,12 +1,19 @@
 const router = require('express').Router()
 const { signUpController, 
         loginUserController,
-        googleLoginController
+        googleLoginController,
+        authorizeCLIController,
+        updateAuthCodeController,
+        verifyTokenController
       } = require('../controllers/authController')
+const authMiddleware = require('../middlewares/auth');
 
 router.post('/signUp', signUpController)
 router.post('/login', loginUserController)
 router.post('/googleLogin', googleLoginController)
+router.post('/authorizeCLI', authorizeCLIController)
+router.patch('/updateAuthCode', updateAuthCodeController)
+router.post('/verify', authMiddleware, verifyTokenController)
 
 
 module.exports = router
