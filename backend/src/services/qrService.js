7ee73e355939a56876a764cd88f51c452239b7b2
loganoneal <loganofneal@gmail.com>
2022-11-21 10:39:18 -0500

    fixes to qr service

--- backend/src/services/qrService.js
+++ backend/src/services/qrService.js
@@ -3,35 +3,47 @@ const db = require("../helpers/database")
 const cron = require("node-cron");
 const isReachable = require('is-reachable');
 
+function getQRs(connection, callback) {
+    sqlSearch = `SELECT * FROM QRCodes`
+    search_query = mysql.format(sqlSearch)
 
-validateLinksService = cron.schedule("*/5 * * * *", function () {
+    connection.query(search_query, async (err, result) => {
+        if (err) throw (err)
+        connection.release()    
+
+        callback(null,result);
+    });
+}
+
+function setQRValid(qrID, reachable, connection, url) {
+    const sqlUpdate = `UPDATE QRCodes SET validLink = ? WHERE qrID = ?`;
+    const update_query = mysql.format(sqlUpdate, [reachable, qrID]);
+    connection.query (update_query);
+}
+
+validateLinksService = cron.schedule("*/2 * * * *", function () {
     console.log("---------------------");
     console.log("NODE-CRON: validating all QR links");
     console.log("---------------------");
 
     db.getConnection(async (err, connection) => {
         if (err) throw (err)
-        sqlSearch = `SELECT * FROM QRCodes`
-        search_query = mysql.format(sqlSearch)
 
-        connection.query(search_query, async (err, result) => {
-            if (err) throw (err)
-            connection.release()
-            
+        getQRs(connection, async function(err,qrs) {
+            console.log(qrs);
 
-            for(let i = 0; i < result.length; i++) {
-                var qrID = result[i]['qrID'];
-                var validLink = result[i]['validLink'];
-                var url = result[i]['qrURL'];
+            for(let i = 0; i < qrs.length; i++) {
+                var qrID = qrs[i]['qrID'];
+                var validLink = qrs[i]['validLink'];
+                var url = qrs[i]['qrURL'];
                 var reachable = await isReachable(url);
 
                 if(reachable != validLink) {
-                    const sqlUpdate = `UPDATE QRCodes SET validLink = ? WHERE qrID = ?`;
-                    const update_query = mysql.format(sqlUpdate, [reachable, qrID]);
-                    await connection.query (update_query);
+                    setQRValid(qrID, reachable, connection, url);
                 }
             }
-        })
+        });
+
     })
 });
 
